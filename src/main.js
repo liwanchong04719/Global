12f5a4efdaabcbac5991a9baa5b817da0c8d3afb
ChenXiao <chenxiaoxiao@navinfo.com>
2017-09-30 10:14:02 +0800

    解决语法错误

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import App from './App';
 import router from './router';
-import VueResource from 'vue-resource';
+
+
 Vue.use(VueResource);
 
 
@@ -14,5 +16,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
 });
